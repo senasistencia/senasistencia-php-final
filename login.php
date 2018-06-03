46b0baad4d4410eeb9b3fef4d5d099c569a93903
David Rugeles <rugeles.david@gmail.com>
2018-06-03 15:02:42 -0500

    se hace modificaciones en el login y se agregan librerias compatibles con el framework de diseño

--- login.php
+++ login.php
@@ -1,22 +1,26 @@
 <!DOCTYPE html>
-  <html>
-    <head>
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-     <!-- <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/> estilos locales-->
-    <!-- CDN solo funciona con internet-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+<html>
+	<head>
+		<!--Import Google Icon Font-->
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<!--Import materialize.css-->
+		<!-- <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/> estilos locales-->
+	<!-- CDN solo funciona con internet-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </head>
+		<!--Let browser know website is optimized for mobile-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	</head>
 
-    <body>
-    <div class="row">
-		
-		
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col s12 blue">
+
+				</div>
+			</div>
 		</div>
+		
 
 
 
@@ -34,11 +38,11 @@
 
 
 
-      <!--CDN solo funciona con internet -->
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
-      <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-     <!--libreria local
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-      <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script> -->
-  </body>
-  </html>
+		<!--CDN solo funciona con internet -->
+		<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+		<!--libreria local
+		<script type="text/javascript" src="js/materialize.min.js"></script>
+		<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script> -->
+</body>
+</html>
