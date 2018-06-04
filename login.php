e2cc80df82dd26af1716dbf77e106187b8a994d0
David Rugeles <rugeles.david@gmail.com>
2018-06-03 22:33:00 -0500

    se hace termino del login e inicio del archivo estilos.css

--- login.php
+++ login.php
@@ -5,22 +5,39 @@
 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 		<!--Import materialize.css-->
 		<!-- <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/> estilos locales-->
-	<!-- CDN solo funciona con internet-->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
-
-		<!--Let browser know website is optimized for mobile-->
+		<!-- CDN solo funciona con internet-->
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+		<link rel="stylesheet" href="css/estilos.css">
 	</head>
 
 	<body>
 		<div class="container">
 			<div class="row">
-				<div class="col s12 blue">
-
-				</div>
+				<form action="" class="card col s12 m6 push-m3 login z-depth-1 grey lighten-4">
+					<div class="section">
+						<img class="logo-login" src="imagenes/logo-login.png" alt="perra">
+					</div>
+					<div class="divider"></div>
+					<div class="section">	
+						<div class="row">
+							<input class="col s8 offset-s2 validate" type="number" placeholder="Numero de identificación" required="true">
+							<input class="col s8 offset-s2 validate" type="password" placeholder="Contraseña" required="true">
+							<button class="btn waves-effect waves-light col s4 offset-s4 z-depth-0 cyan darken-3" type="submit">Ingresar</button>
+						</div>						
+						<div class="divider"></div>
+						<div class="section center-align">
+							<a href="" >Recuperar contraseña</a>
+						</div>
+					</div>
+				</form>
 			</div>
 		</div>
-		
+  <div class="footer">
+		<strong>© 2018 SENASISTENCIA</strong>		
+	</div>
+
 
 
 
