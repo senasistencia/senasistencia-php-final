53995cfeeb134bc36d177a19a19d5754fc19ad9a
David Rugeles <rugeles.david@gmail.com>
2018-06-04 21:42:09 -0500

    se hace el inicio de usuario se crea el archivo header.php

--- vistas/usuario/index.php
+++ vistas/usuario/index.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-		<!--Import Google Icon Font-->
-		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-		<!--Import materialize.css-->
-		<!-- <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/> estilos locales-->
-		<!-- CDN solo funciona con internet-->
-  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<?php
+  require("header.php");
 
-		<link rel="stylesheet" href="../../css/estilos.css">
-</head>
-<body>
+?>
   <nav>
     <div class="nav-wrapper cyan darken-4">
       <img src="../../imagenes/admin.png" class="logo-admin brand-logo">
